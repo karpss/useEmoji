53f3902797d9780c62ae4c6808dc95c3f3c4b033
karpss <ola.fam10@gmail.com>
2022-02-16 14:39:34 +0100

    Implemted emoji picker without bugs

--- src/components/EmojiList.js
+++ src/components/EmojiList.js
@@ -1,70 +1,58 @@
 /* eslint-disable */
 import React, {useState, useEffect} from 'react';
-import { fetchEmoji } from '../api';
-
-
-
-
-
+import {fetchEmoji} from '../api';
 
 const EmojiList = () => {
-  
-const [emoji, setEmoji] = useState([]);
-const [hideEmoji, setHideEmoji] = useState(true);
-const [input, setInput] = useState('');
+  const [emoji, setEmoji] = useState([]);
+  const [hideEmoji, setHideEmoji] = useState(true);
+  const [input, setInput] = useState('');
 
-useEffect(() => {
-  const getEmojiList = async () => {
-    setEmoji(await fetchEmoji());
+  useEffect(() => {
+    const getEmojiList = async () => {
+      setEmoji(await fetchEmoji());
+    };
+    getEmojiList();
+  }, [setEmoji]);
+
+  const handleClick = (e) => {
+    console.log('Target', e.target.innerHTML);
+    const eTarget = e.target.innerHTML;
+
+    setInput((prevInput) => prevInput + eTarget);
   };
-      getEmojiList();
-      
-    }, [setEmoji]);
-
-
-    //console.log(emoji);
-  
-  const handleClick = () => {
-    //console.log('Working');
-    //setInput(prevInput => prevInput + emojiObject);
-    const selectedValue = emoji.filter((emojis) => emojis)
-      setInput(prevInput => prevInput + selectedValue);
-      //console.log(event.target.dataset.user)
-    
-  }
 
   
 
   return (
-    
-    
-  <div>
-    
-    
-   <form>
-   
-   
-      <p>
-        <label htmlFor="comment">Comment:</label>
-        <textarea
-          name="comments"
-          value={input}
-          onChange={event => setInput(event.target.value)}
-          type="text"  
-        ></textarea>
-      </p>      
-      
-    </form>
-    
-      {!hideEmoji ?<div className='emojilist'>
-       <p  onClick={handleClick} >{ emoji.map(number => <>{number}</>) }</p>
-    </div> : null }
-    <button onClick={() => setHideEmoji(s => !s)}>Add Emoji</button>
-    
+    <div>
+      <form>
+        <p>
+          <label htmlFor="comment">Comment:</label>
+          <textarea
+            name="comments"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            type="text"
+          />
+        </p>
+      </form>
+
+      {!hideEmoji ? (
+        <div className="emojicontainer">
+          <div onClick={handleClick}>
+            {emoji.map((icon, i) => (
+              <p key={i} className="emojilist">
+                {icon}
+              </p>
+            ))}
+          </div>
+        </div>
+      ) : null}
+      <button type="button" onClick={() => setHideEmoji((s) => !s)}>
+        Add Emoji
+      </button>
     </div>
-    
-  
-  )
+  );
 };
 
 export default EmojiList;
